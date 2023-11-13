8ce4c6eb5f1198d6a3b9a32ec991c7e6c25fe091
sumrak10 <nurlan.urazkulov@gmail.com>
2023-11-13 13:07:50 +0500

    1) Верстку формы отправки заявки - сделал согласно рекомендациям Маркетолога
    2) Переносы строк в описании товара, теперь так же работают и на странице товара
    
    3) Таблицы на главной странице разъезжались из-за многострочного названия категории, я это поправил, теперь все карточки выглядят одинаково.
    
    4) Теперь можно создавать копии товаров. Это делается так же как и выставляется скидка у товаров.
    Подробнее: Из главного меню переходим в список всех товаров. выбираем нужный(ые) товар(ы) и в меню действий выбираем 'Создать копию', для того чтобы не потерять копию созданного товара следует вписать в поиск название этого товара, и тогда будут отображаться только товары с одинаковыми именами.

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib import messages
 from django.utils.translation import ngettext
+from django.db.models.query import QuerySet
 
 from .models import Category, Product, ProductImage, Review, ProductCharacteristic, ReviewImages, ProductOption, Basket, ProductInBasket
 
@@ -51,8 +52,55 @@ class ProductCharacteristicsInstanseInline(admin.TabularInline):
     extra = 1
     model = ProductCharacteristic
     exclude = ['its_articul']
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+
+    @admin.action(description='Создать копию')
+    def create_copy(self, request, queryset: QuerySet[Product]):
+
+        # Проход по queryset и создание копий
+        for obj in queryset:
+            images = ProductImage.objects.filter(product=obj)
+            options = ProductOption.objects.filter(product=obj)
+            characteristics = ProductCharacteristic.objects.filter(product=obj)
+            categories = obj.categories.all()
+
+            obj.id = None
+            obj.save()
+            
+            obj.categories.set(categories)
+            obj.save()
+            
+            for image in images:
+                image.id = None
+                image.product = obj
+                image.save()
+
+            for option in options:
+                option.id = None
+                option.product = obj
+                option.save()
+
+            for characteristic in characteristics:
+                characteristic.id = None
+                characteristic.product = obj
+                characteristic.save()
+            
+
+        count = queryset.count()
+        self.message_user(
+            request,
+            ngettext(
+                "Создана копия %d товара",
+                "Создана копия %d товаров",
+                count,
+            )
+            % count,
+            messages.SUCCESS,
+        )
+
     @admin.action(description='Убрать скидку')
     def change_discount_to_0(self, request, queryset):
         updated = queryset.update(discount='0')
@@ -66,6 +114,7 @@ class ProductAdmin(admin.ModelAdmin):
             % updated,
             messages.SUCCESS,
         )
+
     @admin.action(description='Изменение скидки на 10')
     def change_discount_to_10(self, request, queryset):
         updated = queryset.update(discount='10')
@@ -79,6 +128,7 @@ class ProductAdmin(admin.ModelAdmin):
             % updated,
             messages.SUCCESS,
         )
+
     @admin.action(description='Изменение скидки на 30')
     def change_discount_to_30(self, request, queryset):
         updated = queryset.update(discount='30')
@@ -92,6 +142,7 @@ class ProductAdmin(admin.ModelAdmin):
             % updated,
             messages.SUCCESS,
         )
+
     @admin.action(description='Изменение скидки на 50')
     def change_discount_to_50(self, request, queryset):
         updated = queryset.update(discount='50')
@@ -120,6 +171,7 @@ class ProductAdmin(admin.ModelAdmin):
             % updated,
             messages.SUCCESS,
         )
+
     @admin.action(description='Сделать товар новинкой')
     def make_new(self, request, queryset):
         updated = queryset.update(new=True)
@@ -133,13 +185,33 @@ class ProductAdmin(admin.ModelAdmin):
             % updated,
             messages.SUCCESS,
         )
+
+    def _create_copy_recursive(self, obj, id_mapping):
+        # Проход по всем полям модели
+        for related_object in obj._meta.related_objects:
+            related_manager = getattr(obj, related_object.get_accessor_name())
+            related_objects = related_manager.all()
+            
+            for related_obj in related_objects:
+                # Если связанный объект уже скопирован, используем новый идентификатор
+                if related_obj.pk in id_mapping:
+                    related_manager.add(related_obj.__class__.objects.get(pk=id_mapping[related_obj.pk]))
+                else:
+                    # Создаем копию связанного объекта
+                    related_obj.id = None
+                    related_obj.save()
+                    new_related_id = related_obj.id
+                    id_mapping[related_obj.pk] = new_related_id
+                    related_manager.add(related_obj)
+
+        obj.save()
     
             
     list_display = ["name","average_reviews","wholesale_price","retail_price","discount","prio"]
     list_filter = ["categories","discount", "new"]
     search_fields = ["name"]
     readonly_fields = ["old_price"]
-    actions = [divider_action1, IncDecPrioMixin.decrease_product_prio, IncDecPrioMixin.increase_product_prio, divider_action2, remove_from_new, make_new, divider_action3, change_discount_to_0, change_discount_to_10, change_discount_to_30, change_discount_to_50]
+    actions = [create_copy, divider_action1, IncDecPrioMixin.decrease_product_prio, IncDecPrioMixin.increase_product_prio, divider_action2, remove_from_new, make_new, divider_action3, change_discount_to_0, change_discount_to_10, change_discount_to_30, change_discount_to_50]
     inlines = [ProductImagesInstanseInline, ProductCharacteristicsInstanseInline, ProductOptionInstanseInline]
 
     
@@ -164,3 +236,5 @@ class ProductInBasketInstanseInline(admin.TabularInline):
 class BasketAdmin(admin.ModelAdmin):
     list_display = ["unique_id", "created_at"]
     inlines = [ProductInBasketInstanseInline]
+
+
