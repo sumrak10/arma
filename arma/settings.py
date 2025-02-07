ee6d834cd09871dba97b04a5a8879ebc7879ede8
sumrak10 <nurlan.urazkulov@gmail.com>
2025-02-07 16:13:05 +0500

    impl: migration to mysql

--- arma/settings.py
+++ arma/settings.py
@@ -108,6 +108,7 @@ DATABASES = {
         'PORT': '3306',
         'OPTIONS': {
             'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",  # Принудительно задаём режим SQL
         },
     }
 }
