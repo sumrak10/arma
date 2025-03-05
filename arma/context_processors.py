25fa20e168336ef07a5375263cd18a3f59c5651b
sumrak10 <nurlan.urazkulov@gmail.com>
2025-03-05 12:28:36 +0500

    impl: address in hero block dynamic

--- arma/context_processors.py
+++ arma/context_processors.py
@@ -1,4 +1,4 @@
-from main.models import Partners, SiteConfiguration
+from main.models import Partners, SiteConfiguration, Manufactory
 import config
 from . import utils
 
@@ -7,9 +7,11 @@ def base_dependencies(request):
     site_config = SiteConfiguration.objects.first()
     partners = Partners.objects.all()
     company_working_now = utils.company_working_now()
+    manufactory = Manufactory.objects.filter(id=1).first()
     return {
         "RECAPTCHA_PUBLIC_KEY": config.RECAPTCHA_PUBLIC_KEY,
         "site_config": site_config,
         "partners": partners,
-        "company_working_now": company_working_now
+        "company_working_now": company_working_now,
+        "main_manufactory": manufactory,
     }
